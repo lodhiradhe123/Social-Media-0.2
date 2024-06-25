a620c9b9a9c5d36bc97693ec8051a4cb40639e47
Radheshyam Lodhi <>
2024-06-26 00:19:37 +0530

    environment variable setup(.env)

--- models/connect.js
+++ models/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://radheshyamlodhi48:uy1m25TQ8TlK0lf5@cluster0.plr1rcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("connected");
  }).catch((err)=>{
     console.log(err);
